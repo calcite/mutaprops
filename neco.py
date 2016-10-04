aff33f5dddcb58282b6fcd6ccd513d251b52f32a
JosefNevrly <jnevrly@alps.cz>
2016-10-04 09:59:30 +0200

    Test update.

--- neco.py
+++ neco.py
@@ -8,7 +8,7 @@ import sys
 logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 @mutaprop_class("Test object")
-class Test(object):
+class Neco(object):
 
     def __init__(self, speed, tire_pressure, name):
         self._speed = speed
@@ -34,21 +34,23 @@ class Test(object):
         """Lowest measured tire pressure across all four wheels."""
         return self._tire_pressure
 
-    @tire_pressure.setter(min_value=1.0, max_value=3.5, step=0.2)
+    @tire_pressure.setter(min_val=1.0, max_val=3.5, step=0.2)
     def tire_pressure(self, value):
         self._tire_pressure
 
 
 def main():
 
-    test = Test(3, 2.23, "Auto1")
-    test2 = Test(3, 2.23, "Auto2")
-    test.muta_init("Id instance")
+    test = Neco(3, 2.23, "Auto1")
+    test2 = Neco(3, 2.23, "Auto2")
+    test.muta_init("Id instance1")
     test2.muta_init("Id instance2")
     print("\n")
 
     for prop_id, prop in test.props.items():
         print("{0}\n------".format(prop))
+        print(prop.to_dict(obj=test))
+        print("\n---------")
 
     print(test.get_class_name())
 
