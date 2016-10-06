872b358637667ef36b8e3b80ef7183739ed578a5
JosefNevrly <jnevrly@alps.cz>
2016-10-06 18:06:07 +0200

    Started working on the web ui.

---
+++ web_ui/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <muta-prop></muta-prop>
+  </div>
+</template>
+
+<script>
+import MutaProp from './MutaProp.vue'
+
+export default {
+  components: { MutaProp },
+  data () {
+    return {
+      msg: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ web_ui/src/MutaProp.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="mutaprop">
+        <span>{{ propName }}</span>
+        <span>{{ propValue }}</span>
+        <p>Jeste neco</p>
+        <alert type="success">Povedlo se.</alert>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                propName: 'Name of the property',
+                propValue: 'A value like no other'
+            }
+        }
+    }
+</script>
