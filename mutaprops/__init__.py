2cc7edbd85474cdce417339bdc4d26bf54e988c3
JosefNevrly <jnevrly@alps.cz>
2016-11-03 18:42:37 +0100

    Fix - added static files to manifest.

--- mutaprops/__init__.py
+++ mutaprops/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Josef Nevrly"""
 __email__ = 'jnevrly@alps.cz'
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .decorators import mutaprop_class, mutaproperty, mutaprop_action, \
     mutaselect, mutaselect_classproperty
