26c7eab98f069fb38ba2e0c80c14531b8c7ae771
JosefNevrly <jnevrly@alps.cz>
2016-11-03 18:29:00 +0100

    Bumped to 0.1.2

--- mutaprops/__init__.py
+++ mutaprops/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Josef Nevrly"""
 __email__ = 'jnevrly@alps.cz'
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 from .decorators import mutaprop_class, mutaproperty, mutaprop_action, \
     mutaselect, mutaselect_classproperty
