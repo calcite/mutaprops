188840a0008d58bc73ba07e815a5df7e52d92309
Josef <jnevrly@alps.cz>
2020-01-08 14:14:10 +0100

    Fixed travis settings.

--- mutaprops/__init__.py
+++ mutaprops/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Josef Nevrly"""
 __email__ = 'jnevrly@alps.cz'
-__version__ = '0.7.1'
+__version__ = '0.7.2'
 
 from .decorators import mutaprop_class, mutaproperty, mutaprop_action, \
     mutasource
