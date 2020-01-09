212c0676b22f10f1c01a894a760c286340d83353
Josef <jnevrly@alps.cz>
2020-01-09 10:49:31 +0100

    Another Travis update.

--- mutaprops/__init__.py
+++ mutaprops/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """Josef Nevrly"""
 __email__ = 'jnevrly@alps.cz'
-__version__ = '0.7.2'
+__version__ = '0.7.3'
 
 from .decorators import mutaprop_class, mutaproperty, mutaprop_action, \
     mutasource
